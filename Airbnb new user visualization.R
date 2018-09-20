0a69a8a7f226e78e8b224c0b69458c15bbaa8d6e
daisyworld2018 <39037277+daisyworld2018@users.noreply.github.com>
2018-09-19 21:20:08 -0700

    Update Airbnb new user visualization.R

--- Airbnb new user visualization.R
+++ Airbnb new user visualization.R
@@ -1,14 +1,23 @@
-train_user_visual <- read.csv(file="train_user_clean.csv", header=TRUE, sep=";")
- 
- #view relationship between age and country_destination
- train_user_visual <- subset(train_user_visual,age < 100 & age > 17)
- ggplot(train_user_visual,aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "stack")
- 
- #remove US, NDF, and other for better visualization of oversea destination choices
- train_user_overseas <- subset(train_user_visual, country_destination != "US" & country_destination != "NDF" & country_destination != "other" )
- ggplot(train_user_overseas,aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "stack")
- ggplot(train_user_overseas,aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "fill")
- 
+library(tidyverse)
+#read in the data
+load('working_data.Rdata')
+train <- read_csv2("train_user_clean.csv")
+glimpse(train)
+colSums(is.na(train))
+
+#subset age lower than 100 and larger than 17
+train1 <- train %>% filter(age > 17 & age < 100)
+summary(train1$age)
+ggplot(train1, aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "stack")
+ggplot(train, aes(x = country_destination)) + geom_bar()
+
+#subset country international locations only
+train2 <- train1 %>% filter(country_destination != "US" & country_destination!= "NDF" & country_destination != "other")
+
+ggplot(train2,aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "stack")
+ggplot(train2,aes(x = age, fill= country_destination)) + geom_histogram(binwidth = 1, position = "fill")
+
+
  
  
  
